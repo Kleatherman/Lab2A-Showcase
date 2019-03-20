7aee395a9999c4c4e09735466dfcb353d3d47c7c
Kyle <kleatherman1@ycp.edu>
2019-03-19 20:06:56 -0400

    First Lab Complete

---
+++ CS320_Lab02_kleatherman1/junit/edu/ycp/cs320/lab02/controller/GuessingGameControllerTest.java
@@ -0,0 +1,50 @@
+package edu.ycp.cs320.lab02.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ycp.cs320.lab02.controller.GuessingGameController;
+import edu.ycp.cs320.lab02.model.GuessingGame;
+
+public class GuessingGameControllerTest {
+	private GuessingGame model;
+	private GuessingGameController controller;
+	
+	@Before
+	public void setUp() {
+		model = new GuessingGame();
+		controller = new GuessingGameController();
+		
+		model.setMin(1);
+		model.setMax(100);
+		
+		controller.setModel(model);
+	}
+	
+	@Test
+	public void testNumberIsGreater() {
+		int currentGuess = model.getGuess();
+		controller.setNumberIsGreaterThanGuess();
+		assertTrue(model.getGuess() > currentGuess);
+	}
+	@Test
+	public void testSetNumberFound() {
+		controller.setNumberFound();
+		assertEquals(model.getMax(),model.getMin());
+	}
+	@Test
+	public void testLessThanGuess() {
+		int temp=model.getGuess();
+		controller.setNumberIsLessThanGuess();
+		assertEquals(model.getMax(),temp);
+	}
+	@Test
+	public void testGreaterThenGuess() {
+		int temp= model.getGuess();
+		controller.setNumberIsGreaterThanGuess();
+		assertEquals(temp,model.getMin());
+	}
+}
---
+++ CS320_Lab02_kleatherman1/junit/edu/ycp/cs320/lab02/model/GuessingGameTest.java
@@ -0,0 +1,45 @@
+package edu.ycp.cs320.lab02.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ycp.cs320.lab02.model.GuessingGame;
+
+public class GuessingGameTest {
+	private GuessingGame model;
+	
+	@Before
+	public void setUp() {
+		model = new GuessingGame();
+	}
+	
+	@Test
+	public void testSetMin() {
+		model.setMin(1);
+		assertEquals(1, model.getMin());
+	}
+	@Test
+	public void testSetMax() {
+		model.setMax(100);
+		assertEquals(100, model.getMax());
+	}
+	@Test
+	public void testisDone() {
+		model.isDone();
+		assertEquals(model.getMax(),model.getMin());
+	}
+	@Test
+	public void testisLessThan() {
+		int temp= model.getGuess();
+		model.setIsLessThan(temp);
+		assertEquals(model.getMax(),temp-1);
+	}
+	@Test
+	public void testisGreaterThan() {
+		int temp= model.getGuess();
+		model.setIsGreaterThan(temp);
+		assertEquals(model.getMin(),temp+1);
+	}
+}
